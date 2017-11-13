2518a39fcdf55d61aad0fe82388d50f2921fe5bf
opilane <opilane@KLASSB315-3>
2017-11-13 14:58:37 +0200

    Array
    Array

---
+++ Arvutustabel/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ajad = new string[] { "kevad", "suvi", "sügis", "talv" };
+            Console.WriteLine($"{ajad[1]} {ajad[3]}");
+            Console.ReadLine();
+        }
+    }
+}
