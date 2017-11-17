3dddc573ef45d2aa5d26818ec590f87c8b584d5b
Virk0 <virko.veelmaa@tptlive.ee>
2017-11-17 13:50:06 +0200

    Esimene

---
+++ tunnitöö 17.11.17/Tunnitöö 17.11.17/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tunnitöö_17._11._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
