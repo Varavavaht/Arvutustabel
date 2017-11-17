5c33d2f71b9770aced0800271fc4efd816303484
Virk0 <virko.veelmaa@tptlive.ee>
2017-11-17 15:09:34 +0200

    Commit

--- tunnitöö 17.11.17/Tunnikontroll/Program.cs
+++ tunnitöö 17.11.17/Tunnikontroll/Program.cs
@@ -10,6 +10,29 @@ namespace Tunnikontroll
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Taco Palenque");
+            Console.WriteLine("1200 Main ST");
+            Console.WriteLine("------------------------------------------------------------------------------");
+            double summa;
+            bool hinnad = true;
+            while (hinnad)
+            {
+                {
+                    Console.Write("Enter price of food item, [-1 to quit]: ");
+                    string input = Console.ReadLine();
+                    double i = double.Parse(input);
+
+                    if (i == -1)
+                    {
+                        break;
+                    }
+                    
+                }        
+            }
+
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine();
         }
+
     }
 }
